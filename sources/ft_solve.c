ed331393c86d214e8c14eddb458a6e4c455f90f8
rofeldsp <igordmkr@gmail.com>
2020-03-09 16:12:08 +0300

    48

--- sources/ft_solve.c
+++ sources/ft_solve.c
@@ -69,6 +69,7 @@ void	ft_solve(t_push **node1, int *fd)
 	out = combine_rotations(&out);
 	display_output(out, (*node1)->debug_opt, fd);
 	free_output(out);
+	free(node2);
 }
 
 int		stack_length(t_push *node)
